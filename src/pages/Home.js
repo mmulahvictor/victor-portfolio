bdc99c7be78f8374e3f7bf97041ae69bc1497fcf
mmulahvictor <mmulahvictor@gmail.com>
2024-09-27 10:56:30 +0300

    Adding content to pages

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+import Hero from '../components/Hero';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
